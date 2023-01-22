0021a1d52a6f8fc9bf11463ceb9b84d98d19f4ec
DmitryPinegin <snobmsi@gmail.com>
2023-01-22 13:17:55 +0300

    update

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,8 +1,11 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
+
 public class Cli {
 
     public static void nameRequest() {
-        Engine.userNameScanner();
+          Engine.userNameScanner();
     }
 }
--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -12,7 +12,7 @@ public class Engine {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         userName = sc.next();
-        System.out.println("Hello, " + userName  + " !");
+        System.out.println("Hello, " + userName  + "!");
         return userName;
     }
 
